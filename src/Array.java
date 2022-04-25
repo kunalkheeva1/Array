5e1945abca6c2efa64928d90126d74ca6a50e468
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-24 22:06:57 -0700

    kya gal karde o yaar!

--- src/Array.java
+++ src/Array.java
@@ -1,8 +1,17 @@
-public class Array {
-    public static void main(String[] args) {
-        int[] array ={23,23,34,234,532};
-        for(int i=0; i<array.length; i++){
-            System.out.println("Array of index "+i +" "+ array[i]);
-        }
+class Kunal{
+    String name; int rollno;
+    Kunal(String name, int rollno){
+        this.name = name;
+        this.rollno= rollno;
     }
 }
+public class Array {
+    public static void main(String[] args) {
+    Kunal [] kk= new Kunal[5];
+    kk[0]= new Kunal("Kunal",21);
+    kk[1]= new Kunal("Navreet",22);
+    kk[2]= new Kunal("Kheeva",23);
+        System.out.println(kk);
+    }
+}
+
