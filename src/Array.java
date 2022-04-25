6205fa4ef844b5f022a72fa7de3560563394ea89
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-24 22:34:15 -0700

    commits

--- src/Array.java
+++ src/Array.java
@@ -1,22 +1,11 @@
-import java.util.Arrays;
-
-class Kunal{
-    public String name; public int rollno;
-    Kunal(String name, int rollno){
-        this.name = name;
-        this.rollno= rollno;
-    }
-}
 public class Array {
     public static void main(String[] args) {
-    Kunal [] kk =new Kunal[3];
-    kk[0]= new Kunal("Kunal",21);
-    kk[1]= new Kunal("Navreet",22);
-    kk[2]= new Kunal("Kheeva",23);
-
-    for(int i=0; i<kk.length; i++){
-            System.out.println("Name and Roll number of the candidate is: "+kk[i].name + ", "+kk[i].rollno );
+        int[][] arr ={{1,2,3},{4,5,6},{7,8,9}};
+        for(int i=0; i<=3; i++){
+            for(int j=0; j<=3; j++){
+                System.out.println(arr[i][j]);
+            }
+            System.out.println();
         }
     }
 }
-
