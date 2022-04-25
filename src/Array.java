1294cd93093f2622157200a7ad6dd86a01edebab
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-24 22:51:06 -0700

    commits

--- src/Array.java
+++ src/Array.java
@@ -4,6 +4,8 @@ public class Array {
     }
     public static void main(String[] args){
     int []arr = m1();
-
+    for(int i=0; i<arr.length; i++){
+        System.out.println(arr[i]);
+    }
     }
 }
