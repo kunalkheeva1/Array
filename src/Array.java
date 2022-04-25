1502c0541e8a6713b80043c8a4d889edb3dde108
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-24 21:46:37 -0700

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] array ={23,23,34,234,532};
+        for(int i=0; i<=array.length; i++){
+            System.out.println("Array of index "+i + array[i]);
+        }
+    }
+}
